02dbf04fc5f27e37fb78027b16d31fe88b47ed23
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-12-28 01:07:09 +0530

    fix: add optional chaining to prevent runtime errors and refactor font imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { fetcher } from "@/lib/coingecko.actions";
 
-const geistSans = Geist({
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -40,9 +36,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className="dark">
-      <body
-        className={`lg:px-4 ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`lg:px-4 ${poppins.variable} antialiased`}>
         <Header trendingCoins={trendingCoins} />
         {children}
       </body>
